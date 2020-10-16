8bf481e12ebaf60d180fc5fca752e9e9d5a6f9a0
Jonasz Karwacki <jonaszakr@gmail.com>
2020-10-16 15:12:17 -0700

    Add comments from https://www.electronjs.org/docs/tutorial/quick-start

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+// Import the app and BrowserWindow modules of the electron package to be able
+// to manage application's lifecycle events, as well as create and control
+// browser windows.
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
+  // After that, you define a function that creates a new browser window with
+  // node integration enabled, loads index.html file into this window
+  // and opens Developer Tools.
+  // https://www.electronjs.org/docs/api/browser-window#new-browserwindowoptions
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -13,14 +20,25 @@ function createWindow () {
   win.webContents.openDevTools()
 }
 
+// Create a new browser window by invoking the createWindow function once the
+// Electron application is initialized.
+// https://www.electronjs.org/docs/api/app#appwhenready
 app.whenReady().then(createWindow)
 
+// Add a new listener that tries to quit the application when it no longer has
+// any open windows. This listener is a no-op on macOS due to the operating
+// system's window management behavior.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
+// Add a new listener that creates a new browser window only if when the
+// application has no visible windows after being activated. For example, after
+// launching the application for the first time, or re-launching the already
+// running application.
+// https://support.apple.com/en-ca/guide/mac-help/mchlp2469/mac
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
